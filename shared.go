19d385122e102a3f1befa5e6369c3da92aa8099f
Dorian Heinrichs <dorian.heinrichs@xiamli.com>
2025-01-24 00:23:25 +0100

    Move NewService into shared file

--- shared.go
+++ shared.go
@@ -1,7 +1,9 @@
 package go_nats
 
 import (
+	"github.com/nats-io/nats.go"
 	"github.com/nats-io/nats.go/micro"
+	"log/slog"
 	"time"
 )
 
@@ -61,22 +63,83 @@ func ProcessCallOptions(opts ...CallOption) CallOpts {
 	return options
 }
 
-type ServerOption func(config *micro.Config)
+type ServerOpts struct {
+	instanceID         string
+	timeout            time.Duration
+	WithoutLeaderFns   bool
+	WithoutFollowerFns bool
+}
+
+type ServerOption func(opts *ServerOpts, config *micro.Config)
 
 func WithStatsHandler(handler micro.StatsHandler) ServerOption {
-	return func(config *micro.Config) {
+	return func(_ *ServerOpts, config *micro.Config) {
 		config.StatsHandler = handler
 	}
 }
 
 func WithDoneHandler(handler micro.DoneHandler) ServerOption {
-	return func(config *micro.Config) {
+	return func(_ *ServerOpts, config *micro.Config) {
 		config.DoneHandler = handler
 	}
 }
 
 func WithErrorHandler(handler micro.ErrHandler) ServerOption {
-	return func(config *micro.Config) {
+	return func(_ *ServerOpts, config *micro.Config) {
 		config.ErrorHandler = handler
 	}
+}
+
+func WithServiceVersion(version string) ServerOption {
+	return func(_ *ServerOpts, config *micro.Config) {
+		config.Version = version
+	}
+}
+
+func WithoutLeaderFns() ServerOption {
+	return func(opts *ServerOpts, _ *micro.Config) {
+		opts.WithoutLeaderFns = true
+	}
+}
+
+func WithoutFollowerFns() ServerOption {
+	return func(opts *ServerOpts, _ *micro.Config) {
+		opts.WithoutFollowerFns = true
+	}
+}
+
+func NewService(name string, conn *nats.Conn, impl any, opts ...ServerOption) (micro.Service, *ServerOpts, error) {
+	config := micro.Config{
+		Name:    name,
+		Version: "1.0.0",
+	}
+
+	// Check if the service implements any of the handler interfaces
+	// but do it before applying options, so these can still override the handlers
+	if statsHandler, isStatsHandler := impl.(StatsHandler); isStatsHandler {
+		config.StatsHandler = statsHandler.Stats
+		slog.Debug("Service implements StatsHandler; using service's Stats method")
+	}
+	if doneHandler, isDoneHandler := impl.(DoneHandler); isDoneHandler {
+		config.DoneHandler = doneHandler.Done
+		slog.Debug("Service implements DoneHandler; using service's Done method")
+	}
+	if errHandler, isErrHandler := impl.(ErrHandler); isErrHandler {
+		config.ErrorHandler = errHandler.Err
+		slog.Debug("Service implements ErrHandler; using service's Err method")
+	}
+
+	// Apply options
+	options := new(ServerOpts)
+	for _, opt := range opts {
+		opt(options, &config)
+	}
+
+	// Create the service
+	service, err := micro.AddService(conn, config)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return service, options, nil
 }
